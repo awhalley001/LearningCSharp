f4bd885b3aecbcc315574bfff64b344c2e3481fd
ahalley001 <ahalley001@protonmail.com>
2023-01-12 23:39:00 -0600

    ForEach Assignment

---
+++ Collections/ForEach/ForEach/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace ForEach
+{
+    internal class Program
+    {
+        public static void Main(string[] args)
+        {
+            int[] nums = new int[10];
+
+            for (int i = 0; i < 10; i++)
+            {
+                nums[i] = i * 10;
+            }
+
+            for (int j = 0; j < nums.Length; j++)
+            {
+                Console.WriteLine($"Element {j} = {nums[j]}");
+            }
+            
+            // foreach loop
+            int counter = 0;
+            foreach (int k in nums)
+            {
+                Console.WriteLine($"Element {counter} = {k}");
+                counter++;
+            }
+            
+
+        }
+    }
+}
--- Collections/ValidInput/ValidInput/Program.cs
+++ Collections/ValidInput/ValidInput/Program.cs
@@ -5,7 +5,7 @@ namespace ValidInput
     internal class Program
     {
         public static void Main(string[] args)
-        {
+        { 
             Console.Write("Enter a value: ");
            string firstValue =  Console.ReadLine();
            
@@ -13,16 +13,48 @@ namespace ValidInput
            Console.WriteLine("Press 1 for string.");
            Console.WriteLine("Press 2 for Integer.");
            Console.WriteLine("Press 3 for Boolean.");
-           int dataTypeChoice = Console.Read();
+           string dataTypeChoice = Console.ReadLine();
 
-           switch (dataTypeChoice)
+           int choiceInt = int.Parse(dataTypeChoice);
+           
+           Console.WriteLine(choiceInt);
+           char[] stringArray = firstValue.ToCharArray();
+
+           string dataTypeCorrect = "is a valid";
+           int stringInteger;
+           switch (choiceInt)
            {
                case 1:
-                   
+                   foreach(char k in stringArray)
+                   {
+                       bool a = Char.IsDigit(k);
+                       if (a == true)
+                       {
+                           dataTypeCorrect = "is an invalid";
+                           break;
+                       }
+                   }
+                   Console.WriteLine($"{firstValue} {dataTypeCorrect} string");
                    break;
                case 2:
+                   foreach(char k in stringArray)
+                   {
+                       bool a = Char.IsDigit(k);
+                       if (a != true)
+                       {
+                           dataTypeCorrect = "is an invalid";
+                           break;
+                       }
+                   }
+                   Console.WriteLine($"{firstValue} {dataTypeCorrect} integer");
                    break;
                case 3:
+                   // Console.WriteLine(firstValue);
+                   if (firstValue != "true" || firstValue != "false")
+                   {
+                       dataTypeCorrect = "is an invalid";
+                   }
+                   Console.WriteLine($"{firstValue} {dataTypeCorrect} boolean");
                    break;
            }
 
